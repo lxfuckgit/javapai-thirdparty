49ff378bb9f3a05a142562cf95283692b60224fc
liuxiang <liuxiang@sinafenqi.com>
2019-09-06 11:10:27 +0800

    增加配置：targetUrl

--- javapai-thirdparty-haiguan/src/main/java/com/javapai/thirdparty/haiguan/controller/HaiguanQueryController.java
+++ javapai-thirdparty-haiguan/src/main/java/com/javapai/thirdparty/haiguan/controller/HaiguanQueryController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +16,18 @@ import com.javapai.thirdparty.haiguan.job.HaiguanRepayJob;
 @RestController
 public class HaiguanQueryController {
 	
+	@Value(value = "${haiguan.data.targetUrl}")
+	private String targetUrl;
+	
 	ExecutorService pool = Executors.newFixedThreadPool(3);
 
 	@PostMapping("/haiguan/platDataOpen")
 //	public Map<String, Object> platDataOpen(HttpServletRequest request) {
 	public Map<String, Object> platDataOpen(@RequestParam(value="openReq") String openReq) {
-		/* 处理请求（视情况根据公司环境job化或是mq处理） */
 		JSONObject object = JSONObject.parseObject(openReq);
-		pool.submit(new HaiguanRepayJob(object.getString("orderNo"), object.getString("sessionID")));
+		
+		/* 处理请求（视情况根据公司环境job化或是mq处理） */
+		pool.submit(new HaiguanRepayJob(targetUrl, object.getString("orderNo"), object.getString("sessionID")));
 		
 		/* 返回报文给海关 */
 		Map<String, Object> result = new HashMap<>();
